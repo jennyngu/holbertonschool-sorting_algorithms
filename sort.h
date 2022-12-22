9a9434428ba0a302670c8c5a4b07470ace349fa5
Jenny <5704@holbertonstudents.com>
2022-12-22 03:12:54 -0800

    Added header file

---
+++ sort.h
@@ -0,0 +1,33 @@
+#ifndef __SORT_H__
+#define __SORT_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+#endif /* __SORT_H__ */
